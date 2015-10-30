a4726d4640cbaa1a5a9793029640de10dab8ca0e
hiichaki <hiichaki350@gmail.com>
2015-10-30 23:33:19 +0200

    operators class

--- cheat2.h
+++ cheat2.h
@@ -314,3 +314,97 @@ void lk(){
     Beep(417, 500);
     Beep(329, 500);    
 }
+
+
+class Qwe{
+	
+	public:
+		int a,b,*arr;
+		Qwe(){
+			a=1;
+		}
+		
+	friend ostream& operator<<(ostream& os, const Qwe& a){
+		os<<a.a<<" "<<a.b;
+    	return os;
+	}
+	friend istream& operator>>(istream& is, Qwe& a){
+	    is>>a.a>>a.b;
+	    return is;
+	}
+	friend bool operator==(Qwe a,Qwe b){
+		return a.a==b.a;
+	}
+	friend bool operator>(Qwe a,Qwe b){
+		return a.a>b.a;
+	}
+	friend bool operator<(Qwe a,Qwe b){
+		return a.a<b.a;
+	}
+	friend bool operator!=(Qwe a,Qwe b){
+		return a.a!=b.a;
+	}
+	friend bool operator>=(Qwe a,Qwe b){
+		return !(a<b);
+	}
+	friend bool operator<=(Qwe a,Qwe b){
+		return !(a>b);
+	}
+	void operator++(int qq){
+		 a++;
+	}
+	void operator++(){
+		 ++a;
+	}
+	void operator--(int qq){
+		 a--;
+	}
+	void operator--(){
+		 --a;
+	}
+	friend Qwe operator+(Qwe a, Qwe b){
+		a.a+=b.a;
+		return a;
+	}
+	friend Qwe operator-(Qwe a, Qwe b){
+		a.a-=b.a;
+		return a;
+	}
+	friend Qwe operator*(Qwe a, Qwe b){
+		a.a*=b.a;
+		return a;
+	}
+	friend Qwe operator/(Qwe a, Qwe b){
+		a.a/=b.a;
+		return a;
+	}
+	void operator+=(Qwe b){
+		this->a+=b.a;
+	}
+	void operator-=(Qwe b){
+		this->a-=b.a;
+	}
+	void operator*=(Qwe b){
+		this->a*=b.a;
+	}
+	void operator/=(Qwe b){
+		this->a/=b.a;
+	}
+	int& operator[] (int i){
+		return arr[i];
+	} 
+    int operator() (int i){
+		return arr[i]*i;
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
