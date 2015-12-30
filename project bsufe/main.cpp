a42525a9c8e850af3844f340844de65fd9755212
hiichaki <hiichaki350@gmail.com>
2015-12-30 15:02:29 +0200

    try project

--- project bsufe/main.cpp
+++ project bsufe/main.cpp
@@ -1,32 +1,16 @@
 #include <fstream>
 #include <time.h>
-#include <iostream>
 #include <stdlib.h>
+#include <windows.h>
 using namespace std;
 
 ofstream f("1.txt");
 
 tm a;
 
-//void write(int t){
-//	f<<a.tm_hour<<" "<<a.tm_min<<" "<<a.tm_sec<<" "<<t<<" "<<a.tm_hour*3600+a.tm_min*60+a.tm_sec<<"\n";	
-//	a.tm_sec++;
-//	if(a.tm_sec>59){
-//		a.tm_sec=0;
-//		a.tm_min++;
-//	}
-//	if(a.tm_min>59){
-//		a.tm_min=0;
-//		a.tm_hour++;
-//	}		
-//}
-
-ofstream h("h.txt"),m("m.txt"),s("s.txt"),tt("t.txt");
 void write(int t){
-	h<<a.tm_hour<<"\n";
-	m<<a.tm_min<<"\n";
-	s<<a.tm_sec<<"\n";
-	tt<<t<<"\n";
+	f.open("1.txt",ios::app);
+	f<<a.tm_hour<<" "<<a.tm_min<<" "<<a.tm_sec<<" "<<t<<" "<<a.tm_hour*3600+a.tm_min*60+a.tm_sec<<"\n";	
 	a.tm_sec++;
 	if(a.tm_sec>59){
 		a.tm_sec=0;
@@ -35,22 +19,27 @@ void write(int t){
 	if(a.tm_min>59){
 		a.tm_min=0;
 		a.tm_hour++;
-	}	
+	}		
+	f.close();
 }
-
-ifstream ff("1.txt");
 
 int main(){
 	srand(time(NULL));
 	int t=rand()%15,q;
-	for(int i=0;i<10000;++i){		
-		write(t);		
-		q=rand()%3;
-		switch(q){
-			case 0: t++;break;
-			case 1: t--;break;
-			case 2:     break;
-		}		
+	
+	SetTimer(NULL,1,1000,NULL);
+	MSG Msg;
+	
+	while(GetMessage(&Msg,NULL,0,0)){	
+		if(Msg.message==WM_TIMER){			
+			write(t);		
+			q=rand()%3;
+			switch(q){
+				case 0: t++;break;
+				case 1: t--;break;
+				case 2:     break;
+			}	
+		}
 	}
 	f.close();
 	
